da0c3f3007feeab86d5db3080c574cb7d36061f0
Saahil Khemnani <saahil.khemlani@gmail.com>
2023-06-07 14:06:23 +0700

    aliases

---
+++ scripts/large-text.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Large Text
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Makes the text larger for viewing on TV
+
+displayplacer "id:2D7FD796-C340-A3E1-0857-D6964E3302DB res:1280x720 hz:60 color_depth:7 scaling:on origin:(0,0) degree:0"
---
+++ scripts/small-text.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Small Text
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Makes text small for desktop mode
+
+displayplacer "id:2D7FD796-C340-A3E1-0857-D6964E3302DB res:1920x1080 hz:60 color_depth:7 scaling:on origin:(0,0) degree:0"
